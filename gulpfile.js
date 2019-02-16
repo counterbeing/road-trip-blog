906a261227aea42276aa13fb4931ac7d9ccbbbe7
Cory Logan <he@corylogan.com>
2019-02-16 15:44:18 -0600

    beginning to process images

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const { series, src, dest } = require('gulp');
 const flatMap = require('flat-map').default
 const scaleImages = require('gulp-scale-images')
 const imagemin = require('gulp-imagemin');
+const dms2dec =  require('dms2dec')
+const ExifImage = require('exif').ExifImage;
+const globby = require('globby');
+
 
 
 const twoVariantsPerFile = (file, cb) => {
@@ -10,6 +14,40 @@ const twoVariantsPerFile = (file, cb) => {
     const medium = file.clone()
     medium.scale = {maxWidth: 1200, format: 'jpg'}
     cb(null, [small, medium])
+}
+
+const getImageDataFromImage = (image, cb) => {
+  try {
+      new ExifImage({ image }, function (error, exifData) {
+          let dec
+          if(exifData.gps.GPSLatitude) {
+
+          dec = dms2dec(
+            exifData.gps.GPSLatitude, exifData.gps.GPSLatitudeRef,
+            exifData.gps.GPSLongitude, exifData.gps.GPSLongitudeRef,
+          );
+        } else {
+          dec = [null, null]
+        }
+          // console.log(exifData)
+          cb ({
+            date: exifData.exif.DateTimeOriginal,
+            description: exifData.image.ImageDescription,
+            lat: dec[0],
+            lng: dec[1],
+            width: exifData.exif.ExifImageWidth,
+            height: exifData.exif.ExifImageHeight,
+          })
+      });
+  } catch (error) {
+  }
+}
+
+const getImageData = async () => {
+  const paths = await globby(['photos/originals/*.{jpg,JPG}']);
+  paths.forEach((path) => {
+    getImageDataFromImage(path, console.log)
+  })
 }
 
 
@@ -31,4 +69,5 @@ function optimize(cb) {
   cb()
 }
 
-exports.default = series(scale, optimize)
+exports.exif = getImageData;
+exports.default = series(scale, optimize);
