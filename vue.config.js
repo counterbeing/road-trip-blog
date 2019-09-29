26afa76044f013fae13f5e90ee63f1d38a56051e
Cory Logan <he@corylogan.com>
2019-09-28 23:27:23 -0700

    new sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,17 @@
-module.exports = { runtimeCompiler: true }
+const PrerenderSPAPlugin = require("prerender-spa-plugin")
+const path = require("path")
+// const stories = require("../src/components/stories.json")
+// const routes = stories.map(story => `/${story.id}`)
+
+module.exports = {
+  runtimeCompiler: true,
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, "../dist"),
+        routes: ["/"]
+        // routes: ["/", ...routes]
+      })
+    ]
+  }
+}
