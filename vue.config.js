9d4e55f412cf3e51a868e096f5476f8d541fe898
Cory Logan <he@corylogan.com>
2019-10-01 00:15:11 -0700

    fix favicon

--- vue.config.js
+++ vue.config.js
@@ -6,17 +6,15 @@ const path = require("path")
 // const stories = require("../src/components/stories.json")
 // const routes = stories.map(story => `/${story.id}`)
 
-const fetch = require("node-fetch")
-
-async function routes() {
-  const url = "https://road-trip-blog.s3.amazonaws.com/storiesIndex.json"
-  const response = await fetch(url)
-  // .then(res => res.json())
-  // .then(json => console.log(json))
-  const json = response.json()
-  return await json.map(story => `/${story.id}`)
-  // console.log(json.map(story => `/${story.id}`))
-}
+// async function routes() {
+//   const url = "https://road-trip-blog.s3.amazonaws.com/storiesIndex.json"
+//   const response = await fetch(url)
+//   // .then(res => res.json())
+//   // .then(json => console.log(json))
+//   const json = response.json()
+//   return await json.map(story => `/${story.id}`)
+//   // console.log(json.map(story => `/${story.id}`))
+// }
 
 module.exports = {
   runtimeCompiler: true,
@@ -26,7 +24,7 @@ module.exports = {
       new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
       new PrerenderSPAPlugin({
         staticDir: path.join(__dirname, "./dist"),
-        routes: ["/", ...routes()]
+        routes: ["/"]
       })
     ]
   }
