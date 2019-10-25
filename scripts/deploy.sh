231fa8c3e1ebe75cb23420b55d6275caf5fccf64
Cory Logan <he@corylogan.com>
2019-10-25 11:11:38 -0700

    adding deploy as script

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+if [ -z "$(git status --porcelain)" ]; then
+  yarn route-grabber && \
+  NODE_ENV=production yarn build && \
+  git checkout gh-pages && \
+  git rm -rf . && \
+  mv dist/* ./ ; git checkout master -- .gitignore && \
+  echo 'together.corylogan.com' >> CNAME && \
+  git add . && \
+  git commit -am 'rev' && \
+  git push origin gh-pages  && \
+  git checkout master
+else
+  echo "You have uncommited changes. Skipping deploy!"
+fi
